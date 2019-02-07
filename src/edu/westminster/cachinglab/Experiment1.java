3e7d030bbb75a122ef2e0a90bddf3da66944dff0
dither001 <32025687+dither001@users.noreply.github.com>
2019-02-07 10:57:07 -0700

    ARRAYS

--- src/edu/westminster/cachinglab/Experiment1.java
+++ src/edu/westminster/cachinglab/Experiment1.java
@@ -14,6 +14,13 @@ public class Experiment1 {
 		
 		Cache aCache = Cache.builder().name("Cache A").drawingFrom(aMem).lineCount(256).accessTime(20).directMapping().build();
 		Cache bCache = Cache.builder().name("Cache B").drawingFrom(bMem).lineCount(8).accessTime(25).setAssociative(2, ReplacementAlgorithm.LRU).build();
+		
+		MemorySystem aSys = new MemorySystem(aCache);
+		MemorySystem bSys = new MemorySystem(bCache);
+		
+		IntArrayValue arrayA = aSys.allocateIntArray(6144);
+		IntArrayValue arrayB = aSys.allocateIntArray(6144);
+		IntArrayValue arrayC = aSys.allocateIntArray(6144);
 
 	}
 
