81c776bb33d21e52b39aa5f1042a9bec1f8d9c6e
dither001 <32025687+dither001@users.noreply.github.com>
2019-02-07 11:38:34 -0700

    Update Exp.2

--- src/edu/westminster/cachinglab/Experiment2.java
+++ src/edu/westminster/cachinglab/Experiment2.java
@@ -5,6 +5,26 @@ import java.util.Random;
 import edu.westminstercollege.cmpt328.memory.*;
 
 public class Experiment2 {
+	
+	public static void main(String...args) {
+		MainMemory aMem = new MainMemory("System A", 200);
+		MainMemory bMem = new MainMemory("System B", 200);
+
+		Cache aCache = Cache.builder().name("Cache A").drawingFrom(aMem).lineCount(256).accessTime(20).directMapping()
+				.build();
+		Cache bCache = Cache.builder().name("Cache B").drawingFrom(bMem).lineCount(8).accessTime(25)
+				.setAssociative(2, ReplacementAlgorithm.LRU).build();
+
+		MemorySystem aSys = new MemorySystem(aCache);
+		MemorySystem bSys = new MemorySystem(bCache);
+		
+		benchmark(aSys);
+		
+		benchmark(bSys);
+	}
+	
+	
+	
 	private static void benchmark(MemorySystem sys) {
         Random random = new Random();
         IntArrayValue data = sys.allocateIntArray(100000);
