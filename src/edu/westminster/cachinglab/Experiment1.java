e3703761c1a48e6ee33f55f87576e77ff9a8773e
Benjamin <redgames13@gmail.com>
2019-02-13 14:32:41 -0700

    Experiment 2

--- src/edu/westminster/cachinglab/Experiment1.java
+++ src/edu/westminster/cachinglab/Experiment1.java
@@ -14,7 +14,7 @@ public class Experiment1 {
 
 		Cache aCache = Cache.builder().name("Cache A").drawingFrom(aMem).lineCount(256).accessTime(20).directMapping()
 				.build();
-		Cache bCache = Cache.builder().name("Cache B").drawingFrom(bMem).lineCount(8).accessTime(25)
+		Cache bCache = Cache.builder().name("Cache B").drawingFrom(bMem).lineCount(256).accessTime(25)
 				.setAssociative(2, ReplacementAlgorithm.LRU).build();
 
 		MemorySystem aSys = new MemorySystem(aCache);
--- src/edu/westminster/cachinglab/Experiment2.java
+++ src/edu/westminster/cachinglab/Experiment2.java
@@ -9,18 +9,35 @@ public class Experiment2 {
 	public static void main(String...args) {
 		MainMemory aMem = new MainMemory("System A", 200);
 		MainMemory bMem = new MainMemory("System B", 200);
-
-		Cache aCache = Cache.builder().name("Cache A").drawingFrom(aMem).lineCount(256).accessTime(20).directMapping()
-				.build();
-		Cache bCache = Cache.builder().name("Cache B").drawingFrom(bMem).lineCount(8).accessTime(25)
+		MainMemory cMem = new MainMemory("System C", 200);
+		
+		Cache aCache = Cache.builder().name("Cache A").drawingFrom(aMem).lineCount(256).accessTime(25)
 				.setAssociative(2, ReplacementAlgorithm.LRU).build();
-
+		Cache bCache = Cache.builder().name("Cache B").drawingFrom(bMem).lineCount(256).accessTime(25)
+				.setAssociative(8, ReplacementAlgorithm.LRU).build();
+		Cache cCache = Cache.builder().name("Cache C").drawingFrom(cMem).lineCount(256).accessTime(25)
+				.fullyAssociative(ReplacementAlgorithm.LRU).build();
+				
+		
 		MemorySystem aSys = new MemorySystem(aCache);
 		MemorySystem bSys = new MemorySystem(bCache);
+		MemorySystem cSys = new MemorySystem(cCache);
 		
 		benchmark(aSys);
 		
 		benchmark(bSys);
+		
+		benchmark(cSys);
+		
+		System.out.println(" \n");
+		
+		benchmark1(aSys);
+		
+		benchmark1(bSys);
+		
+		benchmark1(cSys);
+		
+		
 	}
 	
 	
@@ -36,6 +53,27 @@ public class Experiment2 {
         for (i.set(0); i.get() < 1000000; i.increment()) {
             a.set(random.nextInt(data.getLength()));
             b.set(random.nextInt(data.getLength()));
+
+            tmp.set(data.get(a));
+            data.set(a, data.get(b));
+            data.set(b, tmp);
+        }
+
+        System.out.printf("\nTotal access time: %,d\n", sys.getTotalAccessTime());
+        sys.printStatistics();
+    }
+	
+	private static void benchmark1(MemorySystem sys) {
+        Random r = new Random();
+        IntArrayValue data = sys.allocateIntArray(100000);
+        IntValue i = sys.allocateInt();
+        IntValue tmp = sys.allocateInt();
+        IntValue a = sys.allocateInt();
+        IntValue b = sys.allocateInt();
+
+        for (i.set(0); i.get() < 1000000; i.increment()) {
+            a.set(data.getLength());
+            b.set(data.getLength());
 
             tmp.set(data.get(a));
             data.set(a, data.get(b));
